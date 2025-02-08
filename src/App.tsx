de2d073072ada80b9444cf7d3ed41a9e41c77600
Septian Rifano <septianinet@gmail.com>
2025-02-08 18:19:20 +0700

    update frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { toast } from "sonner";
+import { Share2 } from "lucide-react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex min-h-screen w-full items-center justify-center p-6">
+      <div className="mx-auto w-full max-w-2xl rounded-2xl bg-white/80 p-8 shadow-lg backdrop-blur-sm transition-all duration-300 hover:shadow-xl">
+        <div className="flex flex-col items-start">
+          <div className="mb-6 text-xl font-medium text-neutral-500 uppercase">
+            Quote of the moment
+          </div>
+          <div className="mb-6 text-4xl font-light text-neutral-600">
+            "This is the quote"
+          </div>
+          <div className="mb-8 text-xl text-neutral-600"> —— Charles Fano</div>
+          <div className="flex w-full items-center justify-between">
+            <button className="rounded-lg bg-black px-4 py-3 text-lg font-bold text-white">
+              New Quote
+            </button>
+            <button>
+              <Share2 />
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
